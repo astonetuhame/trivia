019068ceeb5fcf5837b411846c572720f0b361cc
astonetuhame <astonetuhame@gmail.com>
2022-08-24 12:58:56 +0300

    Test adding new question

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,15 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('postgres:root@localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format(
+            'postgres:root@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        self.new_question = {"question": "what is IDE in full",
+                             "answer": "Intergrated Development Kit",
+                             "category": "2",
+                             "difficulty": 2
+                             }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -24,7 +31,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +40,7 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
     def test_get_paginated_questions(self):
         res = self.client().get("/questions")
         data = json.loads(res.data)
@@ -42,14 +50,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data["questions"])
         self.assertTrue(data["categories"])
         self.assertTrue(len(data["questions"]))
-    
+
     def test_404_sent_requesting_beyond_valid_page(self):
         res = self.client().get("/questions?page=1000")
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data["success"], False)
-        self.assertEqual(data["message"], "resource not found")    
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_create_new_question(self):
+        res = self.client().post("/questions", json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["question"])
+        self.assertTrue(len(data["question"]))
 
 
 # Make the tests conveniently executable
