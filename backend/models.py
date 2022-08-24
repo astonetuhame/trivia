ec99211e05f770c51cafc2a4bef63a1736c56ca0
astonetuhame <astonetuhame@gmail.com>
2022-08-24 12:26:20 +0300

    Refactor

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,8 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('postgres:root@localhost:5432', database_name)
+database_path = 'postgresql://{}/{}'.format(
+    'postgres:root@localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -10,6 +11,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,10 +20,13 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 """
 Question
 
 """
+
+
 class Question(db.Model):
     __tablename__ = 'questions'
 
@@ -54,12 +60,15 @@ class Question(db.Model):
             'answer': self.answer,
             'category': self.category,
             'difficulty': self.difficulty
-            }
+        }
+
 
 """
 Category
 
 """
+
+
 class Category(db.Model):
     __tablename__ = 'categories'
 
@@ -73,4 +82,4 @@ class Category(db.Model):
         return {
             'id': self.id,
             'type': self.type
-            }
+        }
