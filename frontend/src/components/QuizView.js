dadae76f8d1ebb5bd249d3859f3feacf7a1d44da
astonetuhame <astonetuhame@gmail.com>
2022-08-25 13:16:04 +0300

    Update QuizView.js

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -117,7 +117,7 @@ class QuizView extends Component {
                   this.selectCategory({ type: this.state.categories[id].id, id })
                 }
               >
-                {this.state.categories[id].id}
+                {this.state.categories[id].type}
               </div>
             );
           })}
